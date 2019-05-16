4fa01ae475d87928cbefc32813e7fc637131f755
jiangkeyuan <40256761+jiangkeyuan@users.noreply.github.com>
2019-05-17 01:20:15 +0800

    jil

--- src/compile/text/compileText.ts
+++ src/compile/text/compileText.ts
@@ -1,12 +1,32 @@
+import { String } from "lodash";
+
 export class CompileText {
-
-    // private _expReg = /{{[ \n]*(\w*)[ \n]*}}/g;
-    
     public render(el: Element, data: any) {
-        // const els = childNodes(this.$el);
-        // els.forEach((v) => {
-        //     console.log(execAll(this._expReg, v.textContent).map((v) => v[1]));
-        // });
+        console.log(el.children);
+        this.setChildNodes(this.getChildNode(el));
+        console.log(this.getChildNode(el));
     }
-
+    getChildNode(el:any):[Element]{
+        return el.childNodes;
+    }
+    setChildNodes(nodeList:[any]){
+        nodeList.forEach(element => {
+            if(element.hasChildNodes()){
+                this.setChildNodes(element.childNodes)
+            }else{
+                if(!!this.cleark(element.nodeValue)){
+                    console.log(this.cleark(element.nodeValue));
+                }
+                
+            }
+        });
+    }
+    cleark(nodeValue:string):string{
+        if(nodeValue == undefined) return "" 
+        var result= nodeValue.replace(/(^\s*)|(\s*$)/g,"");
+        if(!!result && result.length !==0 && result != undefined){
+            return result;
+        }
+        
+    }
 }
